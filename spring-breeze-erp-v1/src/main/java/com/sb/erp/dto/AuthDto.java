f2556fbd6d44ceecdca41d046c42a1e7e70e4b1c
yoonguri988 <alice951816@gmail.com>
2026-06-11 22:19:39 +0900

    feat: 프로젝트 초기 설정 및 공통 셋업 설정

---
+++ spring-breeze-erp-v1/src/main/java/com/sb/erp/dto/AuthDto.java
@@ -0,0 +1,8 @@
+package com.sb.erp.dto;
+
+import lombok.Data;
+
+@Data
+public class AuthDto {
+	private String auth;
+}
---
+++ spring-breeze-erp-v1/src/main/java/com/sb/erp/security/CustomUser.java
@@ -0,0 +1,36 @@
+package com.sb.erp.security;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import com.sb.erp.dto.AuthUserDto;
+
+import lombok.Getter;
+
+@Getter
+public class CustomUser extends User{
+	private static final long serialVersionUID = 1L;
+	AuthUserDto dto;
+	
+	//1. 유저 아이디와 비밀번호를 받아서 권한이 있는지 체크
+	public CustomUser(String username, 
+				      String password, 
+				      Collection<? extends GrantedAuthority> authorities) {
+		super(username, password, authorities); 
+	}
+ 
+	//2.username, password에 맞게 셋팅
+	public CustomUser(AuthUserDto dto) {
+		super(dto.getEmail(), 
+			  dto.getBpass(), 
+			  dto.getAuthList().stream()
+			                   .map(auth->new SimpleGrantedAuthority(auth.getAuth()))
+			                   .collect(Collectors.toList()));
+		this.dto = dto;
+	}
+	
+}
