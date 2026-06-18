299fc8cf84797f6fa170dc4f672203c694246f08
dndkd97 <dndkd97@naver.com>
2026-06-18 20:25:53 +0900

    프로젝트 기능

--- spring-breeze-erp-v1/src/main/java/com/sb/erp/controller/ProjectController.java
+++ spring-breeze-erp-v1/src/main/java/com/sb/erp/controller/ProjectController.java
@@ -19,12 +19,7 @@ import com.sb.erp.util.PagingUtil;
 public class ProjectController {
 	@Autowired ProjectService service;
 	
-	/*
-	 * @RequestMapping(value="/proj/proj_list.do", method=RequestMethod.GET) public
-	 * String list() { return "proj/proj_list"; }
-	 */
-	
-	@RequestMapping(value="/proj/proj_list.do" , method=RequestMethod.GET) //전체출력시
+	@RequestMapping(value="/proj/proj_list" , method=RequestMethod.GET) //전체출력시
 	public String listselect(Model model, @RequestParam(value="pstartno", defaultValue = "1")int pstartno) {
 		model.addAttribute("paging", new PagingUtil(service.selectCnt(),pstartno));
 		model.addAttribute("list", service.select10(pstartno));
@@ -40,41 +35,45 @@ public class ProjectController {
 	public String insert() {return "proj/proj_create";} //등록
 	
 	
-	@RequestMapping(value="/proj/proj_create.do", method=RequestMethod.POST)
+	@RequestMapping(value="/proj/proj_create", method=RequestMethod.POST)
 	public String insert_post(ProjectDto dto, HttpSession session) { //등록처리
 		
 		/*
-		 * dto.setEmp_id(.getEmp_id)); dto.setCom_id(.getCom_id)); 
+		 	dto.setEmpId((Integer)session.getAttribute("emp_id"));
+    		dto.setComId((Integer)session.getAttribute("com_id"));
+    		 Integer empId = (Integer) session.getAttribute("emp_id");
+    		 Integer comId = (Integer) session.getAttribute("com_id");
 		 */
-		
+		dto.setComId(1);
+		dto.setEmpId(1);
 	   service.insert(dto);
-	  return "redirect:/proj/proj_list.do";
+	  return "redirect:/proj/proj_list";
 	}
 	
-	@RequestMapping(value="/proj/proj_detail.do", method=RequestMethod.GET)
+	@RequestMapping(value="/proj/proj_detail", method=RequestMethod.GET)
 	public String select(int pro_id, Model model) { //상세
 		model.addAttribute("dto",service.select(pro_id));
 		return "proj/proj_detail";
 	}
 	
-	@RequestMapping(value="/proj/proj_edit.do", method = RequestMethod.GET)
+	@RequestMapping(value="/proj/proj_edit", method = RequestMethod.GET)
 	public String editView(int pro_id, Model model) { //수정뷰
 		model.addAttribute("dto", service.editView(pro_id));
 		return "proj/proj_edit";
 	}
 	
-	@RequestMapping(value="/proj/proj_edit.do", method=RequestMethod.POST)
+	@RequestMapping(value="/proj/proj_edit", method=RequestMethod.POST)
 	public String edit_post(ProjectDto dto,RedirectAttributes rttr) { //수정처리
 		String result="수정실패";
 		if(service.edit(dto)>0) {result="수정성공";}
 		rttr.addFlashAttribute("result",result);
-		return "redirect:/proj/proj_detail.do?pro_id="+dto.getPro_id();
+		return "redirect:/proj/proj_detail?pro_id="+dto.getProId();
 	}
 	
-	@RequestMapping(value="/proj/delete.do", method=RequestMethod.GET) //삭제
+	@RequestMapping(value="/proj/delete", method=RequestMethod.GET) //삭제
 	public String delete(int pro_id) {
 		service.delete(pro_id);
-		return "redirect:/proj/proj_list.do";
+		return "redirect:/proj/proj_list";
 	}
 	
 
