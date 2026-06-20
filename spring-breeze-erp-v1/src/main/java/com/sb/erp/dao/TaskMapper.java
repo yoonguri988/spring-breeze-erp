3cce4baa40b0536bd3801d5f9a5e0464e9472e50
최다영 <https://github.com/dndkd97/AI-FULLSTACK.git>
2026-06-20 21:46:22 +0900

    태스크 기능

--- spring-breeze-erp-v1/src/main/java/com/sb/erp/dao/TaskMapper.java
+++ spring-breeze-erp-v1/src/main/java/com/sb/erp/dao/TaskMapper.java
@@ -6,9 +6,18 @@ import com.sb.erp.dto.TaskDto;
 
 @Mapper
 public interface TaskMapper {
+	//태스크 등록
 	public int insert(TaskDto dto);
+	
+	//태스크 멤버 조회
 	public List<TaskDto> selectAll(int pro_id);
+	
+	//태스크 삭제
 	public int delete(int task_id);
+	
+	//태스크 삭제
 	public int update(TaskDto dto);
+	
+	//태스크 상세
 	public TaskDto select(int task_id);
 }
