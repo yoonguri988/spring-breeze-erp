d5b300047db82e3c429a16abaf96634e133ff9dc
최다영 <https://github.com/dndkd97/AI-FULLSTACK.git>
2026-06-20 21:44:27 +0900

    프로젝트 멤버 기능

--- spring-breeze-erp-v1/src/main/java/com/sb/erp/controller/ProjectMemberController.java
+++ spring-breeze-erp-v1/src/main/java/com/sb/erp/controller/ProjectMemberController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.sb.erp.dto.ProjectMemberDto;
 import com.sb.erp.service.ProjectMemberService;
@@ -23,14 +24,19 @@ public class ProjectMemberController {
 		return "proj/proj_member";} //프로젝트 참여인원 조회
 	
 	  @RequestMapping(value="/proj/proj_member_create",method=RequestMethod.POST) 
-	  public String insert(ProjectMemberDto dto) { 
-		  service.insert(dto); 
+	  public String insert(ProjectMemberDto dto,RedirectAttributes rttr) { 
+		  String result="프로젝트 멤버 추가 실패";
+		  if(service.insert(dto)>0) {result="프로젝트 멤버 추가 성공";}
+		  rttr.addFlashAttribute("result",result);
 		  return "redirect:/proj/proj_member?pro_id="+dto.getProjectProId(); } //프로젝트 멤버 추가
 	 
 	
 	@RequestMapping(value="/proj/proj_member_delete",method=RequestMethod.GET) 
-	public String delete(@RequestParam("pm_id")int pm_id, @RequestParam("pro_id") int pro_id ) {
-		service.delete(pm_id);
+	public String delete(@RequestParam("pm_id")int pm_id, @RequestParam("pro_id") int pro_id,
+			RedirectAttributes rttr) {
+		String result="프로젝트 멤버 삭제 실패";
+		if(service.delete(pm_id)>0) {result="프로젝트 멤버 삭제 성공";}
+		rttr.addFlashAttribute("result",result);
 		return "redirect:/proj/proj_member?pro_id="+pro_id;}//프로젝트 멤버 삭제
 
 }
