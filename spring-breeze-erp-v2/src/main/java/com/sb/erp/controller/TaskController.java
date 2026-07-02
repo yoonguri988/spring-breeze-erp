67927e6bb8bf4385d96427dfd200e22696149070
dndkd97 <dndkd97@naver.com>
2026-07-02 18:24:49 +0900

    태스크 멤버 기능 mapping값만 바꿈

--- spring-breeze-erp-v2/src/main/java/com/sb/erp/controller/TaskController.java
+++ spring-breeze-erp-v2/src/main/java/com/sb/erp/controller/TaskController.java
@@ -3,6 +3,8 @@ package com.sb.erp.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,19 +19,20 @@ import com.sb.erp.service.TaskService;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
+@RequestMapping("/proj")
 public class TaskController {
 	@Autowired TaskService service;
 	@Autowired ProjectMemberService memberservice;
 	@Autowired EmpService empservice;
 
-	@RequestMapping(value="/proj/task_create", method=RequestMethod.GET)
+	@GetMapping("/task_create")
 	public String createFrom(@RequestParam int project_pro_id, Model model) {
 		model.addAttribute("memberlist",memberservice.selectByproject(project_pro_id));
 		model.addAttribute("pro_id",project_pro_id);
 		return "proj/task_create"; // 프로젝트 멤버 이름 출력
 	}
 	
-	 @RequestMapping(value="/proj/task_create", method=RequestMethod.POST) 
+	 @PostMapping("/task_create") 
 	 public String create(TaskDto dto,RedirectAttributes rttr, HttpSession session) {
 		 Integer comId = (Integer) session.getAttribute("comId");
 		 dto.setComId(comId); //해당회사의
@@ -47,7 +50,7 @@ public class TaskController {
 		 return "redirect:/proj/proj_detail?pro_id="+dto.getProId();
 	 	} // 태스크 등록
 	
-	  @RequestMapping(value="/proj/task_detail",method=RequestMethod.GET) 
+	  @GetMapping("/task_detail") 
 	 public String view(int task_id,Model model) {
 	 TaskDto dto = service.select(task_id);
 	  model.addAttribute("dto",service.select(task_id));
@@ -55,7 +58,7 @@ public class TaskController {
 	  return "proj/task_detail";
 	  } //해당 태스크 상세
 	 	  
-	 @RequestMapping(value="/proj/task_edit", method=RequestMethod.GET) 
+	 @GetMapping("/task_edit") 
 	 public String taskEditView(int task_id,int project_pro_id, Model model) {
 		  model.addAttribute("dto",service.taskEditView(task_id));
 		  model.addAttribute("memberlist",memberservice.selectByproject(project_pro_id));
@@ -63,7 +66,7 @@ public class TaskController {
 		  return "proj/task_edit";  }//태스크 수정뷰
 		  
 	  
-	  @RequestMapping(value="/proj/task_edit", method=RequestMethod.POST)
+	  @PostMapping("/task_edit")
 	 public String edit(TaskDto dto,RedirectAttributes rttr) {
 		  String result= "태스크 수정 실패";
 		  if(service.update(dto)>0) {result="태스크 수정 성공";}
@@ -71,7 +74,7 @@ public class TaskController {
 			return "redirect:/proj/task_detail?task_id="+dto.getTaskId();
 	  } //태스크 수정폼
 	  
-	  @RequestMapping(value="/proj/task_delete", method=RequestMethod.GET)
+	  @GetMapping("/task_delete")
 	  public String delete(int task_id, @RequestParam("pro_id")int pro_id,RedirectAttributes rttr) {
 		  String result="태스크 삭제 실패";
 		  if(service.delete(task_id)>0) {result="태스크 삭제 성공";}
