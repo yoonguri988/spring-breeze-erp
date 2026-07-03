262730c349cebccf56d1a22ea1b4c8479d7e8d4f
dndkd97 <dndkd97@naver.com>
2026-07-03 18:22:28 +0900

    태스크.mapper.xml 페이징 추가

--- spring-breeze-erp-v2/src/main/java/com/sb/erp/dao/TaskMapper.java
+++ spring-breeze-erp-v2/src/main/java/com/sb/erp/dao/TaskMapper.java
@@ -5,14 +5,16 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.sb.erp.dto.TaskDto;
+import com.sb.erp.dto.TaskSearchDto;
 
 @Mapper
 public interface TaskMapper {
 	//태스크 등록
 	public int insert(TaskDto dto);
 	
-	//태스크 멤버 조회
-	public List<TaskDto> selectAll(int pro_id);
+	//태스크 페이징
+	public List<TaskDto> selectAll(TaskSearchDto search);
+	public int selectCnt (int pro_id);
 	
 	//태스크 삭제
 	public int delete(int task_id);
