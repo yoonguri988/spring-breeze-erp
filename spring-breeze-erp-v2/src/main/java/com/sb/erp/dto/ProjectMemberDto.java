0b28f3849b8bd49a2871db0fa1be67811e51ced7
dndkd97 <dndkd97@naver.com>
2026-07-03 18:23:13 +0900

    프로젝트멤머 dto 수정

--- spring-breeze-erp-v2/src/main/java/com/sb/erp/dto/ProjectMemberDto.java
+++ spring-breeze-erp-v2/src/main/java/com/sb/erp/dto/ProjectMemberDto.java
@@ -1,17 +1,16 @@
 package com.sb.erp.dto;
 
-import java.time.LocalDateTime;
-import java.util.Date;
+import java.time.LocalDate;
 
 import lombok.Data;
 
 @Data
 public class ProjectMemberDto {
-	private int pmId;
-	private int projectProId;
-	private int empId;
-	private String role;
-	private Date joinedAt;
+	private Integer pmId;
+	private Integer projectProId;
+	private Integer empId;
+	private String memberRole;
+	private LocalDate joinedAt;
 	private String empName;
 	private String proName;
 	private String deptName;
