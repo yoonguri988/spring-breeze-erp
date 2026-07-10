2a796fffe006101794559d644408946160fcd716
yoonguri988 <alice951816@gmail.com>
2026-07-10 12:10:26 +0900

    refactor: 파일 업로드 기능 리팩토링

--- spring-breeze-erp-v2/src/main/java/com/sb/erp/dto/ResSearchDto.java
+++ spring-breeze-erp-v2/src/main/java/com/sb/erp/dto/ResSearchDto.java
@@ -23,7 +23,8 @@ public class ResSearchDto {
 	// 검색 조건이 비어있는지 확인 여부
 	public boolean hasSearchCondition() {
 		return (keyword != null && !keyword.isEmpty())
-	            || (resType != null && !resType.isEmpty());
+	            || (resType != null && !resType.isEmpty())
+				|| (resStatus != null && !resStatus.isEmpty());
 	        // 검색 필드 추가될 때마다 여기에 || 조건 추가
 	}
 }
