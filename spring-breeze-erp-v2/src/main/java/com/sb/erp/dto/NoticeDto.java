e90f1fc2a3f747ab53142aca217561e0b0cd2918
dndkd97 <dndkd97@naver.com>
2026-07-10 10:19:03 +0900

    refactor:사원이름 추가,리팩토링

--- spring-breeze-erp-v2/src/main/java/com/sb/erp/dto/NoticeDto.java
+++ spring-breeze-erp-v2/src/main/java/com/sb/erp/dto/NoticeDto.java
@@ -1,5 +1,7 @@
 package com.sb.erp.dto; 
 
+import java.time.LocalDate;
+
 import lombok.Data;
 
 /* Notice 게시글을 표현하는 DTO
@@ -8,18 +10,16 @@ import lombok.Data;
 
 @Data
 public class NoticeDto { //notice
-	private int bno;          // 게시글 번호
-	// private String category;  // 카테고리 (인사, 보안, IT, 복지, 일반)
-	private String btitle;    // 제목
-	private String bcontent;  // 내용
-	private int bhit;         // 조회수
-	private String bfile;     // 서버에 저장할 파일
-	private String createdAt; // 입력한 날짜 (포맷팅용 String 또는 Date)
-	private String updatedAt; // 수정한 날짜
-	private int empId;		  // 사원ID - 공지글 작성자 bwriter
-	private int comId;		  // 회사ID 
-
-
+	private Integer bno;         
+	private String btitle;    
+	private String bcontent;  
+	private Integer bhit;         
+	private String bfile;    
+	private LocalDate createdAt; 
+	private LocalDate updatedAt; 
+	private Integer empId;
+	private Integer comId;
+	private String empName;
 }
 
 
