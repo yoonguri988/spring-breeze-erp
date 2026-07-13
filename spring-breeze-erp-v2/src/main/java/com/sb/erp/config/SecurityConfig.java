1fb1a142e866eaf56ab6170c2f3acecae554b3b5
yoonguri988 <alice951816@gmail.com>
2026-07-13 15:38:56 +0900

    hotfix: @EnableMethodSecurity 부재

--- spring-breeze-erp-v2/src/main/java/com/sb/erp/config/SecurityConfig.java
+++ spring-breeze-erp-v2/src/main/java/com/sb/erp/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,6 +15,7 @@ import com.sb.erp.security.CustomUserDetails;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 	// http 경로 설정
 	@Bean
