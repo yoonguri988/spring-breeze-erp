9544ab4d7b8cc6090d85e0d49f188a07cdaf98a8
sujeong <mdfy0308@gmail.com>
2026-07-14 16:38:54 +0900

    fix : 경로 수정

--- spring-breeze-erp-v2/src/main/java/com/sb/erp/config/SecurityConfig.java
+++ spring-breeze-erp-v2/src/main/java/com/sb/erp/config/SecurityConfig.java
@@ -26,19 +26,20 @@ public class SecurityConfig {
 						.requestMatchers("/css/**", "/js/**", "/images/**", 
 								"/api/**", "/auth/login", "/auth/confirm",
 								"/auth/resetPass", "/auth/forgotResetPass").permitAll()
-						// ─── 로그인만 하면 접근 가능 ───────────
-						.requestMatchers("/auth/updatePass", "/", "/emp/list", "/emp/detail", "/emp/edit",
-								"/emp/editPass", "/com/**", "/dept/**", "/appr/**", "/res/**", "/resv/**", "/proj/**",
-								"/notice/**", "eval/period/list").authenticated()
 						// ─── ROOT 전용 ────────────────────────
 						.requestMatchers("/root/**").hasAuthority("ROOT")
 						// ─── ADMIN 이상 ───────────────────────
 						.requestMatchers("/admin/**").hasAnyAuthority("ROOT", "ROLE_ADMIN")
-						// ─── 사원/직급/권한 관리 (ADMIN 전용) ────────────────
+						// ─── 사원/직급/권한/평가 관리 (ADMIN 전용) ────────────────
 						.requestMatchers("/emp/add", "/emp/resetPass", "/emp/checkEmail", "/emp/checkMobile",
 								"/emp/checkEmpNo", "/perm/**", "/pos/**", "/dept/transfer/pending",
-								"/dept/transfer/list", "/dept/transfer/log", "/admin/**", "/eval/**").hasRole("ADMIN")
-						
+								"/dept/transfer/list", "/dept/transfer/log", "/admin/**", "/eval/report/detail", 
+								"/eval/report/edit", "/eval/report/list", "/eval/period/**", "/eval/list",
+								"/eval/add", "/eval/detail", "/eval/edit").hasRole("ADMIN")
+						// ─── 로그인만 하면 접근 가능 ───────────
+						.requestMatchers("/auth/updatePass", "/", "/emp/list", "/emp/detail", "/emp/edit",
+								"/emp/editPass", "/com/**", "/dept/**", "/appr/**", "/res/**", "/resv/**", 
+								"/proj/**", "/notice/**", "/eval/report/my").authenticated()
 				// ─── 그 외 ────────────────
 				// .anyRequest().permitAll()
 				)
