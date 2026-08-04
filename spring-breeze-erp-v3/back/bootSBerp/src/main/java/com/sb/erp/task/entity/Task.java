90aa0ac73104ed47a44ab7c7db8b77df8c2da178
dndkd97 <dndkd97@naver.com>
2026-08-04 12:51:32 +0900

    refactor:@Entity-Task

--- spring-breeze-erp-v3/back/bootSBerp/src/main/java/com/sb/erp/task/entity/Task.java
+++ spring-breeze-erp-v3/back/bootSBerp/src/main/java/com/sb/erp/task/entity/Task.java
@@ -2,22 +2,29 @@ package com.sb.erp.task.entity;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.sb.erp.com.entity.Company;
 import com.sb.erp.proj.entity.Project;
 import com.sb.erp.proj.entity.ProjectMember;
 
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -26,33 +33,42 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Table(name="TASK")
 public class Task {
 	@Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "task_seq")
-    @SequenceGenerator(name = "task_seq", sequenceName = "TASK_SEQ", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_task")
+    @SequenceGenerator(name = "seq_task", sequenceName = "SEQ_TASK", allocationSize = 1)
 	@Column(name="TASK_ID", nullable = false)
 	private Integer taskId;
 	
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name="PRO_ID", nullable = false)
 	private Project project;
 
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name="PM_ID", nullable = false)
 	private ProjectMember projectMember;
 
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name="COM_ID", nullable = false)
 	private Company company;
 	
-	@Column(name="PARENT_TASK_ID")
-	private Integer parentTaskId;
+	// 부모 태스크
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "PARENT_TASK_ID")
+	private Task parent;
 	
-	@Column(name="TASK_NAME", length=100)
+    // 자식 태스크
+    @OneToMany(mappedBy = "parent")
+    @Builder.Default
+    private List<Task> children = new ArrayList<>();
+	
+	@Column(name="TASK_NAME", length=100, nullable = false)
 	private String taskName;
 	
-	@Column(name="TASK_DESC", length=1000)
+	@Column(name="TASK_DESC", length=1000, nullable = false)
 	private String taskDesc;
 	
 	@Column(name="TASK_STATUS", nullable = false, length=20)
