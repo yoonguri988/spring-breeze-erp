7fd100b96e04f00f15099195dae296b4b9a5dc57
dndkd97 <dndkd97@naver.com>
2026-08-05 20:05:31 +0900

    refactor:주간보고서들Response

--- spring-breeze-erp-v3/back/bootSBerp/src/main/java/com/sb/erp/week/dto/response/WeeklyReportResponse.java
+++ spring-breeze-erp-v3/back/bootSBerp/src/main/java/com/sb/erp/week/dto/response/WeeklyReportResponse.java
@@ -1,12 +1,12 @@
-package com.sb.erp.week.dto;
+package com.sb.erp.week.dto.response;
 
 import java.time.LocalDate;
 
-import lombok.Data;
+import lombok.Getter;
 
-@Data
-public class WeeklyReportDto {
-	private Integer proId;
+@Getter
+public class WeeklyReportResponse {// 팀장용보고서 (프로젝트단위 집계) / 순수 조회/계산
+	private Long proId;
 	private String projectName;
 	
 	//진행 현황
@@ -25,8 +25,12 @@ public class WeeklyReportDto {
 	private LocalDate endDate; 
 	
 	//ai에게 넘길 정보
-	private long remainDays;
+	private Long remainDays;
 	
 	//역할별 보고서
 	private String reportRole;
+	
+	public void setRemainDays(long remainDays) {
+		this.remainDays = remainDays;
+	}
 }
