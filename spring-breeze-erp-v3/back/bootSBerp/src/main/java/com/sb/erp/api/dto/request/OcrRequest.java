2c344ee278b4511ab96452d3f73efe65ccf41a35
yoonguri988 <alice951816@gmail.com>
2026-08-06 15:06:45 +0900

    fix: DTO 파일 작성 및 검토

--- spring-breeze-erp-v3/back/bootSBerp/src/main/java/com/sb/erp/api/dto/request/OcrRequest.java
+++ spring-breeze-erp-v3/back/bootSBerp/src/main/java/com/sb/erp/api/dto/request/OcrRequest.java
@@ -1,21 +1,22 @@
 package com.sb.erp.api.dto.request;
 
+import java.util.List;
+
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.List;
-
 @Getter @Setter
 public class OcrRequest {
-    private String version = "V2";
-    private String requestId;
-    private long timestamp;
-    private List<ImageDto> images;
+	private final String version = "V2"; // 외부 OCR API 스펙 고정값 - 변경 불가
 
-    @Getter @Setter
-    public static class ImageDto {
-        private String format; // jpg, png 등
-        private String name;   // 이미지 명
-        private String data;   // Base64 인코딩된 이미지 문자열
-    }
+	private String requestId;
+	private long timestamp;
+	private List<ImageDto> images;
+
+	@Getter @Setter
+	public static class ImageDto {
+		private String format; // jpg, png 등
+		private String name;   // 이미지 명
+		private String data;   // Base64 인코딩된 이미지 문자열
+	}
 }
