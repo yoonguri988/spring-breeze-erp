7373f81179c1bd5d11d4bdf702d2641c4314799f
yoonguri988 <alice951816@gmail.com>
2026-08-14 18:18:59 +0900

    fix : test 프젝 스웨거는 됨

---
+++ spring-breeze-erp-test/back/src/main/java/com/sb/erp/global/config/JacksonConfig.java
@@ -0,0 +1,17 @@
+package com.sb.erp.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper()
+                .registerModule(new JavaTimeModule());
+    }
+}
